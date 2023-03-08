e0fff5d9bcd6488eaa2b53acd0d68c5ea82fa9d5
Aws-star <75694518+Aws-star@users.noreply.github.com>
2023-03-08 17:47:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+    let sumAll = 0;
+  for (let i = fromN; i <= toN; i ++) {
+    sumAll += i;
+  }
+  return sumAll;
 }
 
 module.exports = sum;
